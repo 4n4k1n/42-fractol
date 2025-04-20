5e12655f15a1949636c5aa913c0e37129f1e90f3
4n4k1n <anakin@pregitzer.de>
2025-04-20 11:47:56 +0200

    function to check the arguments

---
+++ src/arguments.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arguments.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 10:55:14 by apregitz          #+#    #+#             */
+/*   Updated: 2025/04/20 11:46:24 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../fractol.h"
+#include <unistd.h>
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+	return (0);
+}
+
+int	check_arguments(int ac, char **av)
+{
+	if (ac < 2 || ac > 4)
+	{
+		write(1, "Invalid amount of arguments!\n", 29);
+		return (1);
+	}
+}
