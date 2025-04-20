0b009fa6c0a6d5e3e747b1689a7072631739d23b
4n4k1n <anakin@pregitzer.de>
2025-04-20 11:53:04 +0200

    updated the print_fractol function, pasing the whole data struct to it

--- src/main.c
+++ src/main.c
@@ -6,37 +6,40 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 21:00:05 by anakin            #+#    #+#             */
-/*   Updated: 2025/04/14 10:55:34 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/20 11:44:34 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-void	print_fractol(mlx_image_t *img, t_zoom *zoom)
+// int	is_valid(t_cords cords, t_zoom zoom)
+// {
+// 	if ((cords.x > zoom.view_x ))
+// }
+
+void	print_fractol(t_data *data)
 {
 	t_cords		cords;
-	t_maped		map;
 	t_complx	c;
 	t_complx	z;
 
-	init_mandelbrot_structs(&map, &z);
-	// if (zoom->scale != 1.0 || zoom->view_x != 0.0 || zoom->view_y != 0.0)
-	// 	calculate_zoomed_map(zoom, &map);
+	z.imag = 0;
+	z.real = 0;
 	cords.y = 0;
 	while (cords.y < HEIGHT)
 	{
 		cords.x = 0;
 		while (cords.x < WIDTH)
 		{
-			c = calc_zoomed_pix(zoom, cords);
-			clac_pixel(z, c, cords, img);
+			c = calc_zoomed_pix(&data->zoom, cords);
+			clac_pixel(z, c, cords, data);
 			cords.x++;
 		}
 		cords.y++;
 	}
 }
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	t_data	data;
 
@@ -49,7 +52,10 @@ int	main(void)
 		return (free(data.mlx), free(data.img), 1);
 	init_zoom(&data.zoom);
 	mlx_key_hook(data.mlx, key_zoom_hook, &data);
-	print_fractol(data.img, &data.zoom);
+	// mlx_key_hook(data.mlx, key_move_hook, &data);
+	// mlx_loop_hook(data.mlx, &key_zoom_hook, &data);
+	// mlx_loop_hook(data.mlx, &key_move_hook, &data);
+	print_fractol(&data);
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
 	return (0);
