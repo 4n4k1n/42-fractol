b770c8aa56ccbfde72449e647c27de128381c82a
4n4k1n <anakin@pregitzer.de>
2025-04-20 11:55:46 +0200

    added rgb struct adn removed old unused functions

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 20:43:01 by anakin            #+#    #+#             */
-/*   Updated: 2025/04/14 10:53:47 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/20 11:47:00 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@
 # include <math.h>
 
 # ifndef WIDTH
-#  define WIDTH 800
+#  define WIDTH 500
 # endif
 
 # ifndef HEIGHT
-#  define HEIGHT 800
+#  define HEIGHT 500
 #endif
 
 # ifndef MAX_ITER
-#  define MAX_ITER 100
+#  define MAX_ITER 256
 # endif
 
 typedef struct s_complx
@@ -43,14 +43,6 @@ typedef struct s_cords
 	int	y;
 }		t_cords;
 
-typedef struct s_maped
-{
-	double	min_x;
-	double	max_x;
-	double	min_y;
-	double	max_y;
-}			t_maped;
-
 typedef struct s_zoom
 {
 	double	scale;
@@ -58,27 +50,35 @@ typedef struct s_zoom
 	double	view_y;
 	int		win_width;
 	int		win_height;
-	int		mouse_status;  
+	int		mouse_status;
 }			t_zoom;
+
+typedef struct s_rgb
+{
+	int			r;
+	int			g;
+	int			b;
+	uint32_t	color;
+}		t_rgb;
 
 typedef struct s_data
 {
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 	t_zoom		zoom;
+	t_rgb		rgb;
 }				t_data;
 
-t_complx	calculate_c(t_maped map, t_cords cords);
+int			check_arguments(int ac, char **av);
 t_complx	calc_zoomed_pix(t_zoom *zoom, t_cords cords);
-void		calculate_zoomed_map(t_zoom *zoom, t_maped *map);
 void		init_zoom(t_zoom *zoom);
 void		zoom_in(t_cords *mouse, t_zoom *zoom);
 void		zoom_out(t_cords *mouse, t_zoom *zoom);
 t_complx	calc_next_iter_num(t_complx z, t_complx c);
-void		init_mandelbrot_structs(t_maped *map, t_complx *complxx_num);
-void		clac_pixel(t_complx z, t_complx c, t_cords cords, mlx_image_t *img);
-void		print_fractol(mlx_image_t *img, t_zoom *zoom);
+void		clac_pixel(t_complx z, t_complx c, t_cords cords, t_data *img);
+void		print_fractol(t_data *data);
 
 void		key_zoom_hook(mlx_key_data_t keydata, void *param);
+void		key_move_hook(mlx_key_data_t keys, void *param);
 
 #endif
