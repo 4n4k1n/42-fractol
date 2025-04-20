b45e4f62ba27ec36f21efb32af4ffe8877d8f8a9
4n4k1n <anakin@pregitzer.de>
2025-04-20 17:36:37 +0200

    added the resize hook and changed the defined sizes in print_fractol to the resizeble in the struct

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 21:00:05 by anakin            #+#    #+#             */
-/*   Updated: 2025/04/20 15:53:12 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/20 17:22:16 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ void	print_fractol(t_data *data)
 	z.imag = 0;
 	z.real = 0;
 	cords.y = 0;
-	while (cords.y < HEIGHT)
+	while (cords.y < data->zoom.win_height)
 	{
 		cords.x = 0;
-		while (cords.x < WIDTH)
+		while (cords.x < data->zoom.win_width)
 		{
 			data->func_ptr(data, cords, &z, &c);
 			cords.x++;
@@ -39,13 +39,14 @@ int	main(int ac, char **av)
 
 	if (!check_arguments(ac, av, &data))
 		return (1);
-	mlx_set_setting(MLX_MAXIMIZED, true);
-	data.mlx = mlx_init(WIDTH, HEIGHT, "Fractal", false);
+	mlx_set_setting(MLX_MAXIMIZED, false);
+	data.mlx = mlx_init(WIDTH, HEIGHT, "Fractol", true);
 	if (!data.mlx)
 		return (1);
 	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 	if (!data.img || (mlx_image_to_window(data.mlx, data.img, 0, 0) < 0))
 		return (free(data.mlx), free(data.img), 1);
+	mlx_resize_hook(data.mlx, resize_window, &data);
 	init_zoom(&data.zoom);
 	mlx_key_hook(data.mlx, key_zoom_hook, &data);
 	print_fractol(&data);
