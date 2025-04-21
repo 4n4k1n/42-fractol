d776147ec1a2f88739681e0ab35d202cecffd94b
4n4k1n <anakin@pregitzer.de>
2025-04-21 19:39:37 +0200

    changed the julia function to get the set data from the static of the get function

--- src/func_ptr.c
+++ src/func_ptr.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 13:28:57 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/20 13:56:19 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/21 16:51:09 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ void mandelbrot(t_data *data, t_cords cords, t_complx *z, t_complx *c)
 void julia(t_data *data, t_cords cords, t_complx *z, t_complx *c)
 {
 	*z = calc_zoomed_pix(&data->zoom, cords);
-	c->real = -0.7;
-	c->imag = 0.27015;
+	c = get_set();
+	clac_pixel(*z, *c, cords, data);
 }
