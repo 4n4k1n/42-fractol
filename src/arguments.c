bf486ed964a54fbf7997ceaf7f4f5062191e2cac
4n4k1n <anakin@pregitzer.de>
2025-04-21 19:35:23 +0200

    added the julia set andeling for the arguments

--- src/arguments.c
+++ src/arguments.c
@@ -6,36 +6,63 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 10:55:14 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/20 16:10:35 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/21 18:57:26 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
+#include <stdio.h>
 
-int	ft_strcmp(const char *s1, const char *s2)
+t_complx	*get_set(void)
 {
-	int i;
+	static t_complx set;
 
-	i = 0;
-	while (s1[i] && s2[i])
+	return (&set);
+}
+
+void	init_set(double real, double imag)
+{
+	t_complx *set;
+
+	set = get_set();
+	set->imag = imag;
+	set->real = real;
+}
+
+int	init_julia_set(int ac, char **av, t_data *data)
+{
+	double real;
+	double imag;
+
+	real = 0;
+	imag = 0;
+	data->func_ptr = julia;
+	if (ac == 3 || ac == 4)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		i++;
+		if (!ft_atof(av[2], &real))
+			return (write(1, "Invalid argument!\n", 18), 0);
+		if (ac == 4)
+		{
+			if (!ft_atof(av[3], &imag))
+				return (write(1, "Invalid argument!\n", 18), 0);
+			init_set(real, imag);
+		}
+		else
+			init_set(real, 0.27015);
 	}
-	if (s1[i] != s2[i])
-		return (s1[i] - s2[i]);
-	return (0);
+	else
+		init_set(-0.7, 0.27015);
+	return (1);
 }
 
 int check_arguments(int ac, char **av, t_data *data)
 {
-	if (ac < 2 || ac > 2)
+	if (ac < 2)
 		return (write(1, "Invalid amount of arguments!\n", 29), 0);
 	else if (ft_strcmp("mandelbrot", av[1]) == 0)
 		data->func_ptr = mandelbrot;
 	else if (ft_strcmp("julia", av[1]) == 0)
-		data->func_ptr = julia;
+		return (init_julia_set(ac, av, data)	);
 	else
 		return (write(1, "Fractol not found!\n", 19), 0);
 	return (1);
