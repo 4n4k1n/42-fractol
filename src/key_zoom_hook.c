f59a34905bd9952b3ac212504f8796c4d8927297
4n4k1n <anakin@pregitzer.de>
2025-04-22 04:23:30 +0200

    added key check for the color mode and removed useless double code

--- src/key_zoom_hook.c
+++ src/key_zoom_hook.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:39:51 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/20 17:18:27 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/22 04:16:51 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,13 @@ void	key_zoom_hook(mlx_key_data_t keys, void *param)
 	if (keys.action == MLX_PRESS)
 	{
 		if (keys.key == MLX_KEY_ESCAPE)
-			mlx_close_window(data->mlx);
+			return (mlx_close_window(data->mlx));
 		else if (keys.key == MLX_KEY_EQUAL || keys.key == MLX_KEY_MINUS)
 		{
 			if (keys.key == MLX_KEY_EQUAL)
 				zoom_in(&middle, &data->zoom);
 			else
 				zoom_out(&middle, &data->zoom);
-			mlx_delete_image(data->mlx, data->img);
-			data->img = mlx_new_image(data->mlx, data->zoom.win_width, data->zoom.win_height);
-			mlx_image_to_window(data->mlx, data->img, 0, 0);
-			print_fractol(data);
 		}
 		if (keys.key == MLX_KEY_W || keys.key == MLX_KEY_S || keys.key == MLX_KEY_A || keys.key == MLX_KEY_D)
 		{
@@ -45,10 +41,12 @@ void	key_zoom_hook(mlx_key_data_t keys, void *param)
 				data->zoom.view_x += 15 * data->zoom.scale;
 			else
 				data->zoom.view_x -= 15 * data->zoom.scale;
-			mlx_delete_image(data->mlx, data->img);
-			data->img = mlx_new_image(data->mlx, data->zoom.win_width, data->zoom.win_height);
-			mlx_image_to_window(data->mlx, data->img, 0, 0);
-			print_fractol(data);
 		}
+		if (keys.key == MLX_KEY_R)
+			data->rgb.rainbow *= -1;
+		mlx_delete_image(data->mlx, data->img);
+		data->img = mlx_new_image(data->mlx, data->zoom.win_width, data->zoom.win_height);
+		mlx_image_to_window(data->mlx, data->img, 0, 0);
+		print_fractol(data);
 	}
 }
