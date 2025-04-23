3b49d98d46daf8ad4ec0b62b8ecbab54f524a51b
4n4k1n <anakin@pregitzer.de>
2025-04-23 15:21:08 +0200

    added function to switch the color

--- src/key_zoom_hook.c
+++ src/key_zoom_hook.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 16:39:51 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/22 05:03:37 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/23 14:04:03 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,35 @@ static void	move_keys(mlx_key_data_t keys, t_data *data)
 		data->zoom.view_x -= 15 * data->zoom.scale;
 }
 
+void	increase_color_part(t_color_byte *byte)
+{
+	if (byte->state)
+	{
+		byte->color += 0.05;
+		if (byte->color + 0.05 > 1.0)
+			byte->state = 0;
+	}
+	else
+	{
+		byte->color -= 0.05;
+		if (byte->color - 0.05 < 0.0)
+			byte->state = 1;
+	}
+}
+
+void	color_flow(t_data *data)
+{
+	increase_color_part(&(data->rgb.r));
+	increase_color_part(&(data->rgb.g));
+	increase_color_part(&(data->rgb.b));
+}
+
+static void	color_keys(mlx_key_data_t keys, t_data *data)
+{
+	if (keys.key == MLX_KEY_P)
+		color_flow(data);
+}
+
 void	key_zoom_hook(mlx_key_data_t keys, void *param)
 {
 	t_data	*data;
@@ -46,6 +75,7 @@ void	key_zoom_hook(mlx_key_data_t keys, void *param)
 			return (mlx_close_window(data->mlx));
 		zoom_keys(keys, data, middle);
 		move_keys(keys, data);
+		color_keys(keys, data);
 		if (keys.key == MLX_KEY_R)
 			data->rgb.rainbow *= -1;
 		mlx_delete_image(data->mlx, data->img);
