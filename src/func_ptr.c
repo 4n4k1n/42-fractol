4edeaac2f8b70b3a40c18522b7f19b78ae1ef83c
4n4k1n <anakin@pregitzer.de>
2025-04-24 17:39:53 +0200

    created the function ptr for the burningship fractol

--- src/func_ptr.c
+++ src/func_ptr.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 13:28:57 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/21 16:51:09 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/24 17:34:42 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,23 @@ void julia(t_data *data, t_cords cords, t_complx *z, t_complx *c)
 	*z = calc_zoomed_pix(&data->zoom, cords);
 	c = get_set();
 	clac_pixel(*z, *c, cords, data);
+}
+
+void burningship(t_data *data, t_cords cords, t_complx *z, t_complx *c)
+{
+	int			i;
+	t_complx	temp;
+	*c = calc_zoomed_pix(&data->zoom, cords);
+	z->real = 0;
+	z->imag = 0;
+	i = -1;
+	while (pow(z->real, 2) + pow(z->imag, 2) < 4 && ++i < MAX_ITER)
+	{
+		temp.real = pow(z->real, 2) - pow(z->imag, 2) + c->real;
+		temp.imag = 2 * fabs(z->real * z->imag) + c->imag;
+		z->real = fabs(temp.real);
+		z->imag = fabs(temp.imag);
+	}
+	
+	mlx_put_pixel(data->img, cords.x, cords.y, get_color(i, data));
 }
