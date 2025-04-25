06654eda7b2ab299b85cc35178f61978fe8da11f
4n4k1n <anakin@pregitzer.de>
2025-04-25 22:12:57 +0200

    moved the print_fractol function to the print dir

--- src/main.c
+++ src/main.c
@@ -6,32 +6,11 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 21:00:05 by anakin            #+#    #+#             */
-/*   Updated: 2025/04/25 21:42:16 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/25 22:12:09 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
-
-void	print_fractol(t_data *data)
-{
-	t_cords		cords;
-	t_complx	c;
-	t_complx	z;
-
-	z.imag = 0;
-	z.real = 0;
-	cords.y = 0;
-	while (cords.y < data->zoom.win_height)
-	{
-		cords.x = 0;
-		while (cords.x < data->zoom.win_width)
-		{
-			data->func_ptr(data, cords, &z, &c);
-			cords.x++;
-		}
-		cords.y++;
-	}
-}
 
 int	main(int ac, char **av)
 {
