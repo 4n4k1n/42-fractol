c84990a4db51cf24dbdf83aba49879b635d01c68
4n4k1n <anakin@pregitzer.de>
2025-04-26 23:35:32 +0200

    fixed segfault at teh parsing part

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 10:55:14 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/25 22:46:01 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/26 23:33:57 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 int	check_arguments(int ac, char **av, t_data *data)
 {
 	init_set(-0.7, 0.27015);
-	if (ac < 2)
-		return (write(1, "Invalid amount of arguments!\n", 29), 0);
-	else if (ft_strcmp("mandelbrot", av[1]) == 0)
+	if (ac < 2 || ac > 4)
+	{
+		write(1, "Invalid arguments!\n./fractol <name> <real> <imag>", 49);
+		return (0);
+	}
+	else if (ft_strcmp("mandelbrot", av[1]) == 0 && ac == 2)
 		return (init_mandelbrot(data));
-	else if (ft_strcmp("julia", av[1]) == 0)
+	else if (ft_strcmp("julia", av[1]) == 0 && (ac >= 2 && ac <= 4))
 		return (init_julia_set(ac, av, data));
-	else if (ft_strcmp("burningship", av[1]) == 0)
+	else if (ft_strcmp("burningship", av[1]) == 0 && ac == 2)
 		return (init_burningship(data));
-	return (write(1, "Fractol not found!\n", 19), 0);
+	write(1, "Invalid arguments!\n./fractol <name> <real> <imag>", 49);
+	return (0);
 }
